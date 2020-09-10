899b3852bbc72a9a9d9672715bd019dc6fed9008
Jaco Jansen van Vuuren <jacojvv.hello@gmail.com>
2020-09-11 00:10:09 +0200

    Deploy website - based on 7ea39c5311cef0e0590b51cc49d84ee2fb126f2d

---
+++ 3761b08b.749c175d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{63:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"azure","name":"azure","count":3,"permalink":"/blog/tags/azure"}')}}]);
